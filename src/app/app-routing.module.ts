22ddb173761999d05c20ae588cf75937de69eae4
Morteza Fathnia <fathniamorteza@gmail.com>
2019-08-15 14:00:49 +0430

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  {
+    path: "home",
+    component: HomeComponent,
+    children: [
+      // {path: '', component: OrangeHomeContentComponent},
+      // {path: 'search', component: OrangeSearchComponent},
+      // // {path: 'orders', component: OrangeOrdersComponent},
+      // {path: 'order/:productID', component: OrangeOrderComponent},
+      // {path: 'cart', component: OrangeCartComponent},
+      // {path: 'orders', component: OrangeOrderListComponent, canActivate: [AuthService]},
+      // {path: 'orders/:orderId', component: OrangeOrderViewComponent, canActivate: [AuthService]},
+      // {path: 'transactions', component: OrangeTransactionsComponent, canActivate: [AuthService]},
+      // {path: 'payment/verify/:gateway', component: OrangePaymentVerifyComponent, canActivate: [AuthService]},
+      // {path: 'payment/:paymentType/:orderId', component: OrangePaymentComponent, canActivate: [AuthService]},
+      // {path: 'account', component: OrangeAccountComponent, canActivate: [AuthService]},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CarouselModule } from "ngx-owl-carousel-o";
+import { HomeComponent } from "./components/home/home.component";
+import { CarouselHolderComponentComponent } from "./components/carousel-holder-component/carousel-holder-component.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, CarouselHolderComponentComponent],
+  imports: [BrowserModule, AppRoutingModule, CarouselModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
